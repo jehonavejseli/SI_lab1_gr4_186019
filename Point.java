7f205c547a1c10f2fa0ff09679611f1b86c01dd0
Jehona Vejseli <jehonavejseli@192.168.1.128>
2020-04-10 19:19:51 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,22 +1,59 @@
 class Point {
-	String id
+	String id;
 	double x,y;
-	String boja;
+	
 
 	//TODO add new variable
+	String boja;
 
 	//TODO constructor
+	public Point(String id, double x, double y, String boja) {
+		super();
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.boja = boja;
+	}
 
 	//TODO setters and getters
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
 
+	public String getBoja() {
+		return boja;
+	}
+	public void setBoja(String boja) {
+		this.boja = boja;
+	}
 	public void move (char xDirection, char yDirection) {
 		//TODO
+		char x=xDirection;
+	    char y=yDirection;
+	    String final =x+''+y;
 	}
 
 	public void draw () {
 		//TODO
+		String directions =move();
+        system.out.println(directions);
 	}
-
 
 
 }
